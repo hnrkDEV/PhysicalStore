d87635601ed42efb231d1777964d5d7fbb112541
João Henrique <114232200+hnrkDEV@users.noreply.github.com>
2025-03-04 00:58:02 -0300

    some fix

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const storeRouter = require('./routes/storesRoutes');
-const cors = require('cors');
-
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/stores', storeRouter);
@@ -12,7 +9,7 @@ app.use('/api/stores', storeRouter);
 app.all('*', (req,res, next) => {
     res.status(404).json({
       status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server!`
+      message: `Não foi possível encontrar ${req.originalUrl}!`
     })
   });
 
